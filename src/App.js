0ee1568686ead07a2ad18fc399944b9acadd8d63
tkolsrud <tom.kolsrud@generalassemb.ly>
2022-04-17 14:22:02 -0700

    uppercase/lowercase works, error in instructions code on state.caps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import {useState} from 'react'
 import './App.css';
 
-function App() {
+function App(props) {
+  const tardis = {
+    name: 'Time and Relative Dimension in Space',
+    caps: false,
+  }
+
+  const [state, setState] = useState(tardis)
+
+  const updateCapitalization = () => {
+    console.log('click')
+    const newState = {
+      name: state.caps ? state.name.toLowerCase() : state.name.toUpperCase(),
+      caps: state.caps ? false : true,
+    }
+
+    setState(newState)
+  }
+
   return (
     <div className="App">
-
+      <h3 onClick={updateCapitalization}>{state.name}</h3>
     </div>
   );
 }
